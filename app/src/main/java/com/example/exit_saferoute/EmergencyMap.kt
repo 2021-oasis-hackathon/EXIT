b58a7a00e6c41803ede7fd50ab6a83cf5b21e960
CMS1108 <asv7420@naver.com>
2021-08-17 18:19:52 +0900

    08.17

--- app/src/main/java/com/example/exit_saferoute/EmergencyMap.kt
+++ app/src/main/java/com/example/exit_saferoute/EmergencyMap.kt
@@ -4,8 +4,12 @@ package com.example.exit_saferoute
 import android.graphics.Color
 import android.location.Geocoder
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.protobuf.DescriptorProtos
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
@@ -14,14 +18,32 @@ import com.naver.maps.map.overlay.PathOverlay
 import com.naver.maps.map.util.FusedLocationSource
 
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.log
 
 
 class EmergencyMap : AppCompatActivity(), OnMapReadyCallback {
+    var firestore = FirebaseFirestore.getInstance()
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
+    var testroute: ArrayList<LatLngData> = arrayListOf()
+    var testDouble:Double? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_test)
+
+        firestore.collection("saferoute").get().addOnSuccessListener {
+                result ->
+            // 성공할 경우
+            testroute.clear()
+            for (document in result) {  // 가져온 문서들은 result에 들어감
+                val item = LatLngData(document["lat"] as Double, document["lng"] as Double)
+                testroute.add(item)
+            }
+            Log.d("testD", ""+testroute[1].lat)
+        }
+
+
         val fm = supportFragmentManager
 
         val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
@@ -58,7 +80,8 @@ class EmergencyMap : AppCompatActivity(), OnMapReadyCallback {
         val geocoder = Geocoder(this@EmergencyMap, Locale.KOREAN)
         val results = geocoder.getFromLocationName(addressName, 1)
         val latlng = LatLng(results[0].latitude, results[0].longitude)
-        val result2 = geocoder.getFromLocationName("광주광역시 북구 용봉로 77", 1)
+        val result2 = geocoder.getFromLocationName("광주광역시 북구 중앙동(누문동 115)", 1)
+
         val latlng2 = LatLng(result2[0].latitude, result2[0].longitude)
 
         this.naverMap = naverMap
@@ -79,9 +102,13 @@ class EmergencyMap : AppCompatActivity(), OnMapReadyCallback {
 
         val path = PathOverlay()
         path.color = Color.GREEN
+
         naverMap.addOnLocationChangeListener { location ->
             path.coords = listOf(latlng, LatLng(location.latitude, location.longitude))
             path.map = naverMap
+            val currentLatitude = location.latitude
+            val currentLongitude = location.longitude
+            val currentLatLng = LatLng(currentLatitude, currentLongitude)
         }
 
 
---
+++ app/src/main/java/com/example/exit_saferoute/LatLngData.kt
@@ -0,0 +1,3 @@
+package com.example.exit_saferoute
+
+data class LatLngData(var lat : Double? = null, var lng: Double? = null)
